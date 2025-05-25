9165607d1a1ee395b3913f3a3cceff60f7b917f9
Dushyant Khandelwal <157887714+dushyant4665@users.noreply.github.com>
2025-05-24 23:42:04 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         assetsDir: 'assets',
         emptyOutDir: true,
         sourcemap: true,
+        copyPublicDir: true,
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html')
@@ -21,19 +22,16 @@ export default defineConfig({
                     'three': ['three']
                 },
                 assetFileNames: (assetInfo) => {
-                    const info = assetInfo.name.split('.');
-                    const ext = info[info.length - 1];
                     if (/\.(png|jpe?g|gif|tiff?|webp|svg)$/.test(assetInfo.name)) {
-                        const dir = assetInfo.name.split('/').slice(0, -1).join('/');
-                        return `${dir}/[name][extname]`;
+                        return 'assets/[name][extname]';
                     }
-                    return `assets/[name]-[hash][extname]`;
+                    return 'assets/[name]-[hash][extname]';
                 }
             }
         }
     },
     publicDir: 'public',
-    base: '/',
+    base: '',
     assetsInclude: ['**/*.jpg', '**/*.png', '**/*.tif', '**/*.svg', '**/*.exr'],
     optimizeDeps: {
         include: ['three']
